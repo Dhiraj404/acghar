c9ffe8aa7568f695b56826fc354bc18c6a1ef94f
Dhiraj Bariyait <bariyiatdhiraj05@gmail.com>
2022-09-22 12:11:55 +0545

    First Commit

---
+++ js/brand.js
@@ -0,0 +1,101 @@
+// //you can change the text, but you need to keep same format [yourtext, color];
+// let yourtext = [
+//     ["Dhiraj's ACGHAR", "#ffd166"],
+//     ["ACGHAR", "#7DE2D1"],
+//     ["Services", "#DE3C4B"]
+//   ];
+
+//   let x = 0;
+//   let y = 0;
+//   //how fast typing is
+//   let wait = 300;
+//   //how long you want to text stay before overwriting
+//   let additionalwait = 5;
+//   let txt = yourtext[0][0].split("");
+//   let out = "";
+//   let retyping = setInterval(function () {
+//     document.getElementById("changingText").innerHTML = out;
+//     if (x > txt.length - 1) {
+//     } else {
+//       out += txt[x];
+//     }
+//     x++;
+//     if (x == txt.length + 2 + additionalwait) {
+//       if (y == yourtext.length - 1) {
+//         y = 0;
+//         txt = yourtext[y][0].split("");
+//         out = "";
+//         document.getElementById("changingText").innerHTML = out;
+//         document.getElementById("changingText").style.color = yourtext[y][1];
+//         x = 0;
+//       } else {
+//         y += 1;
+//         txt = yourtext[y][0].split("");
+//         out = "";
+//         document.getElementById("changingText").innerHTML = out;
+//         document.getElementById("changingText").style.color = yourtext[y][1];
+//         x = 0;
+//       }
+//     }
+//   }, wait);
+
+var isAnimating = false;
+
+function scrollLeftAnimate(elem, unit) {
+
+    if (!elem || isAnimating) {
+        //if element not found / if animating, do not execute slide
+        return;
+    }
+
+    var time = 300; // animation duration in MS, the smaller the faster.
+    var from = elem.scrollLeft; // to continue the frame posistion
+    var aframe =
+        10; //fraction of frame frequency , set 1 for smoothest  ~ set 10++ for lower FPS (reduce CPU usage)
+    isAnimating = true; //if animating prevent double trigger animation
+
+    var start = new Date().getTime(),
+        timer = setInterval(function () {
+            var step = Math.min(1, (new Date().getTime() - start) / time);
+            elem.scrollLeft = ((step * unit) + from);
+            if (step === 1) {
+                clearInterval(timer);
+                isAnimating = false;
+            }
+        }, aframe);
+}
+
+
+
+function initDealCarrousel(dealCarrouselID) {
+    var target = document.querySelector("#" + dealCarrouselID + " .va-carrousel-flexbox");
+    var cardOutterWidth;
+    var maxCarrouselScroll;
+
+    function updateUpaCarrouselInfo() {
+        cardOutterWidth = document.querySelector("#" + dealCarrouselID + " .va-card").offsetWidth; //you can define how far the scroll
+        maxCarrouselScroll = (document.querySelectorAll("#" + dealCarrouselID + " .va-card").length *
+            cardOutterWidth) - document.querySelector("#" + dealCarrouselID + " .va-carrousel-flexbox")
+                .clientWidth;
+    }
+
+    document.querySelector("#" + dealCarrouselID + " .deals-scroll-left").addEventListener("click",
+        function () {
+            updateUpaCarrouselInfo(); //in case window resized, will get new info
+            if (target.scrollLeft > 0) {
+                scrollLeftAnimate(target, -cardOutterWidth * 2);
+            }
+        }
+    );
+
+    document.querySelector("#" + dealCarrouselID + " .deals-scroll-right").addEventListener("click",
+        function () {
+            updateUpaCarrouselInfo(); //in case window resized, will get new info 
+            if (target.scrollLeft < maxCarrouselScroll) {
+                scrollLeftAnimate(target, cardOutterWidth * 2);
+            }
+        }
+    );
+}
+// Initiate the container with ID
+initDealCarrousel('va_container'); //carrousel ID
